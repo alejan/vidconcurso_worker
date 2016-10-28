02d7e9684c06fbaf90447884a8efef9b82ed6e15
EC2 Default User <ec2-user@ip-172-31-6-40.us-west-2.compute.internal>
2016-10-28 05:39:39 +0000

    update procfile

--- runner.sh
+++ runner.sh
@@ -4,4 +4,5 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 #cd /home/ec2-user
 # ruby Worker.rb
-/home/ec2-user/.rvm/wrappers/ruby-2.3.0/ruby /home/ec2-user/worker/Worker.rb
+#/home/ec2-user/.rvm/wrappers/ruby-2.3.0/ruby /home/ec2-user/worker/Worker.rb
+ruby /home/ec2-user/worker/Worker.rb
