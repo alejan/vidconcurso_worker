f8fd0b3a7423bf5ed298d0cfc83f9403e157360e
alejan <alejandr0quinter0@yahoo.com>
2016-10-28 20:00:36 -0500

    se agrega sendGrid para envio de correo

--- Worker.rb
+++ Worker.rb
@@ -89,7 +89,7 @@ data = JSON.parse('{
     {
       "to": [
         {
-          "email": vid.items[0]['video_id']
+          "email": vid.items[0]["video_id"]
         }
       ],
       "subject": "vidconcurso"
