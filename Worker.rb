83fd5dc0f8bee34749cdbd66892c64f5522147b2
alejan <alejandr0quinter0@yahoo.com>
2016-10-29 23:32:31 -0500

    se agrega new relic

--- Worker.rb
+++ Worker.rb
@@ -6,7 +6,7 @@ require 'streamio-ffmpeg'
 require 'mail'
 require 'aws-sdk'
 require 'sendgrid-ruby'
-require 'heroku-api'
+require 'platform-api'
 include SendGrid
 
 heroku = Heroku::API.new(ENV['HEROKU_API_KEY'])
