c24f1031140aa2f78f9848ee281f93641dbdd1c9
alejan <alejandr0quinter0@yahoo.com>
2016-10-30 02:55:36 -0500

    se agrega new relic

--- Worker.rb
+++ Worker.rb
@@ -12,7 +12,7 @@ include SendGrid
 heroku = Heroku::API.new(:api_key => '1c0f985e-4bf2-48cc-935d-cc8714c5a17b')
 #heroku = Heroku::API.new(:password => '1c0f985e-4bf2-48cc-935d-cc8714c5a17b')
 #heroku = Heroku::API.new(:headers => {'User-Agent' => 'custom'}) 
-
+heroku.delete_addon('vidconworker', 'papertrail')
 puts  heroku.get_apps
 puts heroku.get_user 
 
