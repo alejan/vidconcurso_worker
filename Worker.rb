3d686c405adb9c712b1dab28edf555dd8ff2f8d3
alejan <alejandr0quinter0@yahoo.com>
2016-10-30 23:55:46 -0500

    primer commit sqs_monitor

--- Worker.rb
+++ Worker.rb
@@ -9,20 +9,24 @@ logger.level = :info
 heroku = Heroku::API.new(:api_key => '1c0f985e-4bf2-48cc-935d-cc8714c5a17b')
 sqs = Aws::SQS::Client.new(region: "us-west-2")
 
+
 qurl=sqs.get_queue_url({
         queue_name: "vidcon_queue"
         })
 
-
-
 resp = sqs.get_queue_attributes({
   attribute_names: ["All"],
-  queue_url: "https://sqs.us-west-2.amazonaws.com/344712433810/vidcon_queue"
+  queue_url: qurl
 })
 
-logger.info resp.attributes['ApproximateNumberOfMessages']
-logger.info heroku.get_app('vidconworker').inspect
+ loop do
 
+logger.info resp.attributes['ApproximateNumberOfMessages']
+logger.info heroku.get_app('vidconworker').body.dynos
+logger.info heroku.get_app('vidconworker').body.workers
+
+sleep 300
+end
 
  
 
